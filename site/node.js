f32ce554a9532738daf0cf4d054d2011427cc3c9
Eduardo Peixoto <luis.00000845392@unicap.br>
2023-05-11 19:17:10 -0300

    site

--- site/node.js
+++ site/node.js
@@ -1,6 +1,5 @@
 https://www.youtube.com/watch?v=7ol2DgfzvC4&list=PLVc5bWuiFQ8FQIWovvUiOO9ok_b6chPsh&index=13
 
-
 // Selecionando elementos do DOM
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav');
@@ -8,7 +7,6 @@ const form = document.querySelector('form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const messageInput = document.querySelector('#message');
-const formSubmitButton = document.querySelector('#submit-button');
 const successMessage = document.querySelector('.success-message');
 
 // Função para exibir/ocultar o menu de navegação
@@ -24,40 +22,14 @@ form.addEventListener('submit', (event) => {
     email: emailInput.value,
     message: messageInput.value
   };
-  // Enviar os dados para um servidor utilizando uma API ou outro método de envio
-  // Neste exemplo, exibimos uma mensagem de sucesso
+  addContact(formData)
   showSuccessMessage();
 });
 
 // Função para exibir a mensagem de sucesso
 function showSuccessMessage() {
-  alert("boa")
   successMessage.style.display = 'block';
   setTimeout(() => {
     successMessage.style.display = 'none';
   }, 5000);
 }
-
-// Função para enviar o formulário de contato
-formSubmitButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  const formData = {
-    name: nameInput.value,
-    email: emailInput.value,
-    message: messageInput.value
-  };
-  // Enviar os dados para o Firestore
-  addContact(formData)
-    .then(() => {
-      // Exibir mensagem de sucesso
-      alert('Mensagem enviada com sucesso!');
-      // Limpar os campos do formulário
-      nameInput.value = '';
-      emailInput.value = '';
-      messageInput.value = '';
-    })
-    .catch((error) => {
-      // Exibir mensagem de erro
-      alert('Ocorreu um erro ao enviar a mensagem: ' + error.message);
-    });
-});
